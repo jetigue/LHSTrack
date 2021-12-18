a72835c8a899ee9aa6dfe46d8dff460299eb70a4
James Tigue <jetigue@comcast.net>
2021-12-18 18:59:32 -0500

    change from from grid to flex on welcome

--- resources/views/livewire/main/welcome.blade.php
+++ resources/views/livewire/main/welcome.blade.php
@@ -3,13 +3,16 @@
         @include('layouts.banner')
     </x-slot>
 
-    <div class="grid grid-cols-6 gap-6  py-4 md:py-8 lg:py-12">
-        <div class="grid col-span-6 lg:col-span-4">
+    <div class="flex w-full flex-wrap py-8">
+        <div class="flex w-full lg:w-2/3 py-2">
             <livewire:main.welcome-page-announcements />
         </div>
 
-        <div class="grid col-span-6 lg:col-span-2">
-            <livewire:main.welcome-page-team-events />
+        <div class="flex w-full lg:w-1/3 py-2 lg:pl-6git ">
+            <div class="w-full">
+                <livewire:main.welcome-page-team-events />
+            </div>
+
         </div>
     </div>
 
