a109065f78abb389e7e4c0f65c5b0fa4c5ea83fd
James Tigue <jetigue@comcast.net>
2021-12-18 19:58:38 -0500

    change logo url

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 <div {{ $attributes->merge(['class' => 'w-20']) }}>
-    <img src="{{ asset('/storage/Logo.png') }}" alt="Longhorn Logo">
+    <img src="{{ asset('/Logo.png') }}" alt="Longhorn Logo">
 </div>
