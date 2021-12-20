9d59f59ecdff420aa0f217f0423243b69be2be98
James Tigue <jetigue@comcast.net>
2021-12-19 21:33:38 -0500

    fix color on roster

--- resources/views/livewire/main/team-roster.blade.php
+++ resources/views/livewire/main/team-roster.blade.php
@@ -4,12 +4,12 @@
 </x-headings.page>
 
 <div>
-    <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4 px-8 text-{{ $athlete->status_color }}-500 font-semibold">
+    <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4 px-8 font-semibold">
         <div class="col-span-1">
             <x-list.ul>
                 <x-slot name="heading">Seniors</x-slot>
                 @foreach($seniors as $senior)
-                    <li>{{ $senior->last_name }}, {{ $senior->first_name }}</li>
+                    <li class="text-{{ $senior->status_color }}-500">{{ $senior->last_name }}, {{ $senior->first_name }}</li>
                 @endforeach
             </x-list.ul>
         </div>
@@ -18,7 +18,7 @@
             <x-list.ul>
                 <x-slot name="heading">Juniors</x-slot>
                 @foreach($juniors as $junior)
-                    <li>{{ $junior->last_name }}, {{ $junior->first_name }}</li>
+                    <li class="text-{{ $junior->status_color }}-500">{{ $junior->last_name }}, {{ $junior->first_name }}</li>
                 @endforeach
             </x-list.ul>
         </div>
@@ -27,7 +27,7 @@
             <x-list.ul>
                 <x-slot name="heading">Sophomores</x-slot>
                 @foreach($sophomores as $sophomore)
-                    <li>{{ $sophomore->last_name }}, {{ $sophomore->first_name }}</li>
+                    <li class="text-{{ $sophomore->status_color }}-500">{{ $sophomore->last_name }}, {{ $sophomore->first_name }}</li>
                 @endforeach
             </x-list.ul>
         </div>
@@ -36,7 +36,7 @@
             <x-list.ul>
                 <x-slot name="heading">Freshmen</x-slot>
                 @foreach($freshmen as $freshman)
-                    <li>{{ $freshman->last_name }}, {{ $freshman->first_name }}</li>
+                    <li class="text-{{ $freshman->status_color }}-500">{{ $freshman->last_name }}, {{ $freshman->first_name }}</li>
                 @endforeach
             </x-list.ul>
         </div>
