88cbff3297b81831549165064da6fbc06c80b331
James Tigue <jetigue@comcast.net>
2022-01-16 19:23:17 -0500

    Update TrackMeetsIndex.php

--- app/Http/Livewire/Meets/TrackMeetsIndex.php
+++ app/Http/Livewire/Meets/TrackMeetsIndex.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Meets;
 
 use App\Models\Meets\TrackMeet;
+use Illuminate\Support\Facades\Route;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -17,6 +18,7 @@ class TrackMeetsIndex extends Component
     public $editing = false;
     public $showFormModal = false;
     public $showConfirmModal = false;
+    public $route;
 
     protected $queryString = ['sortField', 'sortDirection', 'search'];
 
@@ -39,6 +41,11 @@ class TrackMeetsIndex extends Component
         'recordUpdated',
         'refreshTrackMeets'
     ];
+
+    public function mount()
+    {
+        $this->route = Route::currentRouteName();
+    }
 
     public function showFormModal() { $this->showFormModal = true; }
     public function hideFormModal() { $this->showFormModal = false; }
