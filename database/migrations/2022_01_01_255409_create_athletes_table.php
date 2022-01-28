db38dc2647bdf6c38fd6f6928519c4ca3db9bada
James Tigue <jetigue@comcast.net>
2022-01-27 19:48:05 -0500

    add if to athlete schema

--- database/migrations/2022_01_01_255409_create_athletes_table.php
+++ database/migrations/2022_01_01_255409_create_athletes_table.php
@@ -13,24 +13,26 @@ class CreateAthletesTable extends Migration
      */
     public function up()
     {
-        Schema::create('athletes', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name', 50);
-            $table->string('last_name', 50);
-            $table->char('sex', 1);
-            $table->date('dob')->nullable();
-            $table->unsignedSmallInteger('grad_year');
-            $table->char('status', 1);
-            $table->unsignedTinyInteger('track_event_subtype_id')->nullable();
-            $table->unsignedBigInteger('user_id')->nullable();
-            $table->date('physical_expiration_date')->nullable();
-            $table->string('slug')->nullable();
-            $table->timestamps();
+        if (!Schema::hasTable('track_events')) {
+            Schema::create('athletes', function (Blueprint $table) {
+                $table->id();
+                $table->string('first_name', 50);
+                $table->string('last_name', 50);
+                $table->char('sex', 1);
+                $table->date('dob')->nullable();
+                $table->unsignedSmallInteger('grad_year');
+                $table->char('status', 1);
+                $table->unsignedTinyInteger('track_event_subtype_id')->nullable();
+                $table->unsignedBigInteger('user_id')->nullable();
+                $table->date('physical_expiration_date')->nullable();
+                $table->string('slug')->nullable();
+                $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('track_event_subtype_id')->references('id')->on('track_event_subtypes');
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('track_event_subtype_id')->references('id')->on('track_event_subtypes');
 
-        });
+            });
+        }
     }
 
     /**
