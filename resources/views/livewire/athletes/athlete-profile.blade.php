a3b3a140053fc2aa391ad15f4a52ed080258f199
James Tigue <jetigue@comcast.net>
2022-02-13 15:48:18 -0500

    add count

--- resources/views/livewire/athletes/athlete-profile.blade.php
+++ resources/views/livewire/athletes/athlete-profile.blade.php
@@ -12,13 +12,13 @@
     </x-headings.page>
 
     <div class="w-full space-y-10">
-        @if (!$runningEventResults->where('athlete_id', $this->athlete->id))
+        @if (count($runningEventResults->where('athlete_id', $this->athlete->id)) > 0)
             <div>
                 @include('livewire.athletes._profile-running-results')
             </div>
 
         @endif
-        @if (!$fieldEventResults->where('athlete_id', $this->athlete->id))
+        @if (count($fieldEventResults->where('athlete_id', $this->athlete->id)) > 0)
             <div>
                 @include('livewire.athletes._profile-field-results')
             </div>
