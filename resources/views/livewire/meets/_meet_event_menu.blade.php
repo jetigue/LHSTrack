e849e40e1c3538f9f9d0debc572a197d2ef8abf2
James Tigue <jetigue@comcast.net>
2022-02-14 20:48:12 -0500

    one more try

--- resources/views/livewire/meets/_meet_event_menu.blade.php
+++ resources/views/livewire/meets/_meet_event_menu.blade.php
@@ -3,7 +3,7 @@
         Events
     </x-headings.section>
     <ol class="space-y-2 -mt-2 w-full px-4 md:px-0">
-        @foreach($boysEvents->sortBy('track_event_subtype_id')->sortBy('distance_in_meters') as $boysEvent)
+        @foreach($boysEvents->sortBy('distance_in_meters')->sortBy('track_event_subtype_id') as $boysEvent)
             <li>
                 <a href="{{ $this->trackMeet->path() }}/boys/events/{{ $boysEvent->slug }}"
                    class="text-gray-400 hover:text-gray-200"
@@ -12,7 +12,7 @@
                 </a>
             </li>
         @endforeach
-        @foreach($girlsEvents->sortBy('track_event_subtype_id')->sortBy('distance_in_meters') as $girlsEvent)
+        @foreach($girlsEvents->sortBy('distance_in_meters')->sortBy('track_event_subtype_id') as $girlsEvent)
             <li>
                 <a href="{{ $this->trackMeet->path() }}/girls/events/{{ $girlsEvent->slug }}"
                    class="text-gray-400 hover:text-gray-200"
