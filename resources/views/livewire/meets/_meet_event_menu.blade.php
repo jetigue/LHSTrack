3af39defb8dd5452b0c9899d36a279e4907bab3c
James Tigue <jetigue@comcast.net>
2022-02-14 20:46:41 -0500

    fix spacing on events

--- resources/views/livewire/meets/_meet_event_menu.blade.php
+++ resources/views/livewire/meets/_meet_event_menu.blade.php
@@ -2,20 +2,20 @@
     <x-headings.section>
         Events
     </x-headings.section>
-    <ol class="space-y-2 -mt-2 w-full px-4 mx:px-0">
-        @foreach($boysEvents->sortBy('distance_in_meters') as $boysEvent)
+    <ol class="space-y-2 -mt-2 w-full px-4 md:px-0">
+        @foreach($boysEvents->sortBy('track_event_subtype_id')->sortBy('distance_in_meters') as $boysEvent)
             <li>
                 <a href="{{ $this->trackMeet->path() }}/boys/events/{{ $boysEvent->slug }}"
-                   class="text-gray-500 hover:text-gray-300"
+                   class="text-gray-400 hover:text-gray-200"
                 >
                     Boys {{$boysEvent->name}}
                 </a>
             </li>
         @endforeach
-        @foreach($girlsEvents->sortBy('distance_in_meters') as $girlsEvent)
+        @foreach($girlsEvents->sortBy('track_event_subtype_id')->sortBy('distance_in_meters') as $girlsEvent)
             <li>
                 <a href="{{ $this->trackMeet->path() }}/girls/events/{{ $girlsEvent->slug }}"
-                   class="text-gray-500 hover:text-gray-300"
+                   class="text-gray-400 hover:text-gray-200"
                 >
                     Girls {{$girlsEvent->name}}
                 </a>
