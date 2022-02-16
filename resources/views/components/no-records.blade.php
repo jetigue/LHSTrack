bbba8208d093e7301c007556855cd15751b231e4
James Tigue <jetigue@comcast.net>
2022-02-16 12:36:10 -0500

    add gate to missing records

--- resources/views/components/no-records.blade.php
+++ resources/views/components/no-records.blade.php
@@ -4,8 +4,10 @@
     <x-icon.emoji-sad class="h-10 w-10 text-gray-400" />
     <h3 class="mt-2 text-sm font-medium text-gray-700 text-lg">No {{ $missingRecord }}s Found</h3>
     <div class="mt-6">
-        <x-button.add>
-            Add {{ $missingRecord }}
-        </x-button.add>
+        @can('coach')
+            <x-button.add>
+                Add {{ $missingRecord }}
+            </x-button.add>
+        @endcan
     </div>
 </div>
