ed3afa6bece5a18978c2de0d9cb38a6c9d94e4ec
James Tigue <jetigue@comcast.net>
2022-02-24 08:55:07 -0500

    add text color to card with header

--- resources/views/components/card/card-with-header.blade.php
+++ resources/views/components/card/card-with-header.blade.php
@@ -1,10 +1,10 @@
-@props(['color' => 'white'])
+@props(['color' => 'white', 'text' => 'text-gray-800'])
 
 <div {{ $attributes->merge(['class' => 'shadow rounded-lg w-full bg-'.$color]) }}>
     <div class="border-b border-gray-300 px-4 pt-4 pb-3 sm:px-6 text-lg font-semibold text-gray-500">
         {{ $header }}
     </div>
-    <div class="px-4 py-5 sm:p-6">
+    <div class="px-4 py-5 sm:p-6 {{ $text }}">
         {{ $slot }}
     </div>
 </div>
