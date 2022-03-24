6df5b7705b603d723a692d2ae55158fcd32268c5
James Tigue <jetigue@comcast.net>
2022-03-24 15:27:36 -0400

    retry pr with filed events

--- resources/views/livewire/athletes/_profile-field-results.blade.php
+++ resources/views/livewire/athletes/_profile-field-results.blade.php
@@ -31,9 +31,9 @@
                     </div>
                 </div>
                 @foreach($fieldEventResults->where('track_event_id', '=', $fieldTrackEvent->id)->sortByDesc('trackMeet.meet_date') as $result)
-                    <div class="flex space-y-4 items-center @if($result->total_inches + ($result->quarter_inch/4) == $result->athlete->fieldEventResults->where('track_event_id', $result->track_event_id)->max('total_distance')) text-purple-400 @endif">
+                    <div class="flex space-y-4 items-center @if(floatval($result->total_inches + ($result->quarter_inch != null ? ($result->quarter_inch/4) : 0)) == $result->athlete->fieldEventResults->where('track_event_id', $result->track_event_id)->max('total_distance')) text-purple-400 @endif">
                         <div class=" flex w-2/12 justify-center pt-4 items-center">
-                            @if ($result->total_inches + ($result->quarter_inch/4) == $result->athlete->fieldEventResults->where('track_event_id', $result->track_event_id)->max('total_distance'))
+                            @if (floatval($result->total_inches + ($result->quarter_inch != null ? ($result->quarter_inch/4) : 0)) == $result->athlete->fieldEventResults->where('track_event_id', $result->track_event_id)->max('total_distance'))
                                     PR
                             @endif
                         </div>
